3d8f18d2e2bff5a7016a96e3afce05b6645a42b2
Aleksandr Savin <alexo.veto@gmail.com>
2024-02-22 15:33:51 +0700

    some test code...

--- lib/src/main_for_debug.dart
+++ lib/src/main_for_debug.dart
@@ -57,119 +57,145 @@ class MyHomePage extends StatelessWidget {
         }
         return true;
       },
-      child: Scaffold(
-        //
-        // *** The Widget AppBarWithSearchSwitch
-        //
-        appBar: AppBarWithSearchSwitch(
-          customIsSearchModeNotifier: isSearchMode,
-          customTextNotifier: searchText,
-          // animation: (child) => AppBarAnimationSlideDown(child: child, milliseconds: 400),
-          // animation: (child) => AppBarAnimationSlideLeft(child: child, milliseconds: 400),
-          animation: AppBarAnimationSlideLeft.call,
-          // animation: (child) => AppBarAnimationSlideLeft(
-          // milliseconds: 400,
-          // withFade: false,
-          // percents: 0.25,
-          // percents: .80,
-          // child: child,
-          // ),
-          appBarBuilder: (context) {
-            return AppBar(
-              title: Text(title),
-              actions: const [
-                AppBarSearchButton(
-                    // changeOnlyOnSubmit: true,
-                    ),
-                // or
-                // IconButton(onPressed: AppBarWithSearchSwitch.of(context)?startSearch, icon: Icon(Icons.search)),
-              ],
-            );
-          },
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          textSelectionTheme: TextSelectionThemeData(
+              cursorColor: Colors.blue, selectionColor: Colors.red),
         ),
-        //
-        // > Just some random code to react to input from AppBarWithSearchSwitch.
-        //
-        body: Column(
-          children: [
-            AppBarOnEditListener(
-              builder: (BuildContext context, searchText, child) {
-                final totalFound = words
-                    .where((element) => element.contains(searchText))
-                    .length;
+        child: Scaffold(
+          //
+          // *** The Widget AppBarWithSearchSwitch
+          //
+          appBar: AppBarWithSearchSwitch(
+            customIsSearchModeNotifier: isSearchMode,
+            customTextNotifier: searchText,
+            // animation: (child) => AppBarAnimationSlideDown(child: child, milliseconds: 400),
+            // animation: (child) => AppBarAnimationSlideLeft(child: child, milliseconds: 400),
+            animation: AppBarAnimationSlideLeft.call,
+            // animation: (child) => AppBarAnimationSlideLeft(
+            // milliseconds: 400,
+            // withFade: false,
+            // percents: 0.25,
+            // percents: .80,
+            // child: child,
+            // ),
 
-                return totalFound > 0 && searchText != ''
-                    ? Text(
-                        totalFound.toString(),
-                        style: Theme.of(context).textTheme.headlineSmall,
-                        textAlign: TextAlign.center,
-                      )
-                    : Container();
-              },
+            theme: Theme.of(context).copyWith(
+              textSelectionTheme: TextSelectionThemeData(
+                selectionHandleColor: Theme.of(context).splashColor,
+                selectionColor:
+                    Theme.of(context).colorScheme.background.withOpacity(0.5),
+                cursorColor: Theme.of(context).colorScheme.background,
+              ),
+              textTheme: Theme.of(context).textTheme.copyWith(
+                    // hint color
+                    headlineSmall:
+                        Theme.of(context).textTheme.headlineSmall?.copyWith(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall
+                                  ?.color
+                                  ?.withOpacity(0.6),
+                            ),
+                  ),
             ),
-            Expanded(
-              child: GridView.builder(
-                itemCount: words.length * 100,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: MediaQuery.of(context).size.width ~/ 120,
-                  childAspectRatio: 1,
-                ),
-                itemBuilder: (BuildContext context, int i) {
-                  return AppBarOnEditListener(
-                    child: SizedBox(
-                      height: 110,
-                      width: 110,
-                      child: GestureDetector(
-                        onLongPress: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => MyHomePage(
-                                      title: words[i % wordsLength],
-                                    )),
-                          );
-                        },
-                        child: Card(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Center(
-                                  child: Text(
-                                    words[i % wordsLength],
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall,
-                                    textAlign: TextAlign.center,
+            appBarBuilder: (context) {
+              return AppBar(
+                title: Text(title),
+                actions: const [
+                  AppBarSearchButton(
+                      // changeOnlyOnSubmit: true,
+                      ),
+                  // or
+                  // IconButton(onPressed: AppBarWithSearchSwitch.of(context)?startSearch, icon: Icon(Icons.search)),
+                ],
+              );
+            },
+          ),
+          //
+          // > Just some random code to react to input from AppBarWithSearchSwitch.
+          //
+          body: Column(
+            children: [
+              AppBarOnEditListener(
+                builder: (BuildContext context, searchText, child) {
+                  final totalFound = words
+                      .where((element) => element.contains(searchText))
+                      .length;
+
+                  return totalFound > 0 && searchText != ''
+                      ? Text(
+                          totalFound.toString(),
+                          style: Theme.of(context).textTheme.headlineSmall,
+                          textAlign: TextAlign.center,
+                        )
+                      : Container();
+                },
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: words.length * 100,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: MediaQuery.of(context).size.width ~/ 120,
+                    childAspectRatio: 1,
+                  ),
+                  itemBuilder: (BuildContext context, int i) {
+                    return AppBarOnEditListener(
+                      child: SizedBox(
+                        height: 110,
+                        width: 110,
+                        child: GestureDetector(
+                          onLongPress: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => MyHomePage(
+                                        title: words[i % wordsLength],
+                                      )),
+                            );
+                          },
+                          child: Card(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Center(
+                                    child: Text(
+                                      words[i % wordsLength],
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headlineSmall,
+                                      textAlign: TextAlign.center,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    builder: (BuildContext context, searchText, child) {
-                      return Visibility(
-                        visible: words[i % wordsLength].contains(searchText),
-                        child: child ?? Container(),
-                      );
-                    },
-                  );
-                },
+                      builder: (BuildContext context, searchText, child) {
+                        return Visibility(
+                          visible: words[i % wordsLength].contains(searchText),
+                          child: child ?? Container(),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
+          //
+          // > Activate search from random place
+          //
+          // floatingActionButton: AppBarWithSearchFinder(
+          //   builder: (context) => FloatingActionButton(
+          //     onPressed: () => AppBarWithSearchFinder.of(context)?.triggerSearch(),
+          //     tooltip: 'Activate search from random place',
+          //     child: const Icon(Icons.search),
+          //   ),
+          // ),
+          floatingActionButton: const AppBarSearchButton(),
         ),
-        //
-        // > Activate search from random place
-        //
-        // floatingActionButton: AppBarWithSearchFinder(
-        //   builder: (context) => FloatingActionButton(
-        //     onPressed: () => AppBarWithSearchFinder.of(context)?.triggerSearch(),
-        //     tooltip: 'Activate search from random place',
-        //     child: const Icon(Icons.search),
-        //   ),
-        // ),
-        floatingActionButton: const AppBarSearchButton(),
       ),
     );
   }
